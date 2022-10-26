28bed9712657bee1b4cb7cf76278e42e548192ae
srpurba <srpurba@myseneca.ca>
2022-10-26 17:33:54 -0400

    baseURL fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
   url: "https://saminarp-docusaurus.github.io",
-  baseUrl: "/",
+  baseUrl: "/saminarp-docusaurus-sample/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
